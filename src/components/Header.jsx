d09c2f9fe2e2c1c75af0efcbb06a2d827d703fac
KimHue <phue6203@gmail.com>
2024-01-21 23:49:28 +0700

    component 10/1- buoi 1

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import Menu from "./Menu";
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Header</h1>
+            <Menu />
+        </header>
+
+    );
+};
+export default Header;
